e11b47630309bd94ec3003e56dd4cb238acd363a
seohee0112 <ghdtj7@naver.com>
2024-02-20 23:53:21 +0900

    init : 파트너 페이지 초기 구현

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const UserPage = () => {
-    return (
-        <div>
-            파트너페이지입니다.
-        </div>
-    );
+  return <div>파트너페이지입니다.</div>;
 };
 
 export default UserPage;
